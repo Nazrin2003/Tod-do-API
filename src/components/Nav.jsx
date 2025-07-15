02ea0726b1f7c7433cd17c921cc8e69c7021c03f
Nazrin K Faizal <nazrinkf@gmail.com>
2025-07-15 06:16:36 +0530

    To Do

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <div>
          <nav className="navbar navbar-expand-lg bg-danger navbar-dark">
             <div className="container-fluid">
-                <a className="navbar-brand fs-3 fw-bold" href="/">📝 To Do</a>
+                <Link className="navbar-brand fs-3 fw-bold" to="/">📝 To Do</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -14,10 +15,10 @@ const Nav = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Add</a>
+                            <Link className="nav-link active" aria-current="page" to="/">Add</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/view">View</a>
+                            <Link className="nav-link" to="/view">View</Link>
                         </li>
                     </ul>
                     <form className="d-flex" role="search">
